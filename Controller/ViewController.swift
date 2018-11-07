bcdee5150e6d7cad1ae60a8291b59baa4298f732
Evgeniy Ryshkov <eugem@macbook-pro-evgeniy.local>
2018-11-07 23:13:40 +0300

    pasreJSONVar_2() added

--- Controller/ViewController.swift
+++ Controller/ViewController.swift
@@ -28,12 +28,12 @@ class ViewController: UIViewController {
         
     }
 
-    @IBAction func parseJson(_ sender: UIBarButtonItem) {
+    func parseJSONVar_1() {
         if let urlArtist = baseURL.withQueries(query) {
             if let data = try? Data(contentsOf: urlArtist) {
                 
                 let decoder = JSONDecoder()
-
+                
                 do {
                     let jsonArtists = try decoder.decode(Artists.self, from: data)
                     artists = jsonArtists.results
@@ -43,7 +43,29 @@ class ViewController: UIViewController {
                 }
             }
         }
+    }
+    
+    func pasreJSONVar_2() {
+        let task = URLSession.shared.dataTask(with: baseURL.withQueries(query)!) { (data, responce, error) in
+            if let data = data, let string = String(data: data, encoding: .utf8) {
+//                print(string);
+                
+                let jsonDecoder = JSONDecoder()
+                do {
+                    let jsonArtists = try jsonDecoder.decode(Artists.self, from: data)
+                    self.artists = jsonArtists.results
+                    print(self.artists)
+                }catch let error {
+                    print(error)
+                }
+            }
+        }
         
+        task.resume()
+    }
+    
+    @IBAction func parseJsonButtonTapped(_ sender: UIBarButtonItem) {
+        pasreJSONVar_2()
         
     }
 
